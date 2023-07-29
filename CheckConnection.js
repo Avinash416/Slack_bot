0151efa41d33c056fecf1a26c9bd7d2fe8cf07dd
Avinash416 <avijadhav416@gmail.com>
2023-07-29 17:13:10 +0530

    Resolved the import errors

--- CheckConnection.js
+++ CheckConnection.js
@@ -1,7 +1,9 @@
+const { WebClient } = require("@slack/web-api");
+
 //keep alive the connection
 async function sendKeepAlivePing() {
     try {
-        const res = await webClient.apiCall('auth.test');
+        const res = await WebClient.apiCall('auth.test');
         console.log('Keep-alive ping successful:', res);
     } catch (error) {
         console.error('Keep-alive ping error:', error);
